0fac5e77a5b86dd3d346c434793cc943e5d60064
Juliana Morikoshi <julianamorikoshi@gmail.com>
2023-09-14 00:45:22 -0300

    fix: server prompts route name

--- server/src/routes/get-all-prompts.ts
+++ server/src/routes/get-all-prompts.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 
 export async function getAllPromptsRoute(app: FastifyInstance) {
-  app.get('/prompt', async () => {
+  app.get('/prompts', async () => {
     const prompt = await prisma.prompt.findMany()
 
     return prompt
